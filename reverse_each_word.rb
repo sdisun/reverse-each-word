f7aa9c93b936f3e721aa88340b5eb59853114c85
Neil <zooming.in@hotmail.com>
2021-07-12 20:40:51 +1000

    updates

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,18 @@
-def reverse_each_word(string)
-    splitted_array = string.split(" ")
-    new_string = []
-    splitted_array.each do |word|
-        new_string << word.reverse
-    end
-    new_string.join(" ")
-end
+# def reverse_each_word(string)
+#     splitted_array = string.split(" ")
+#     new_string = []
+#     splitted_array.each do |word|
+#         new_string << word.reverse
+#     end
+#     new_string.join(" ")
+# end
 
 
 def reverse_each_word(string)
     splitted_array = string.split(" ")
-    new_string = []
-    splitted_array.collect do |word|
-        new_string << word.reverse
+    new_array = splitted_array.collect do |word|
+        word.reverse
     end
-    new_string.join(" ")
+
+    new_array.join(" ")
 end
